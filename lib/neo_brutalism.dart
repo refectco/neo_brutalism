bf5fb50bd049763a4319171e2d08d6ba2e16beb8
Austin Nelson <therefectcorporation@gmail.com>
2023-08-26 16:05:27 -0400

    Add export for neo_fab component

--- lib/neo_brutalism.dart
+++ lib/neo_brutalism.dart
@@ -1,5 +1,6 @@
 library neo_brutalism;
 
 export './components/neo_container.dart';
+export './components/neo_fab.dart';
 export './components/neo_icon_button.dart';
 export './components/neo_text_field.dart';
